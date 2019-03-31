a0d91513c076d4e10c3aeef4df4b399abaa3f734
sa--sondarkar <saisondarkar18@gmail.com>
2019-03-31 09:25:25 +0530

    initial Commit

---
+++ app/src/main/java/edu/itm/aiforhrrecruit/SplashActivity.java
@@ -0,0 +1,53 @@
+package edu.itm.aiforhrrecruit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    protected int _splashTime = 3000; // time to display the splash screen in ms
+    public boolean is_first = false;
+    public boolean isInterent = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while ((waited < _splashTime)) {
+                        sleep(100);
+//                        if (_active) {
+                        waited += 100;
+//                            if(waited>_splashTime-200)
+//                            {
+//
+////                            }
+//                        }
+                    }
+                } catch (Exception e) {
+
+                } finally {                    {
+
+                    if(isInterent){
+                        startActivity(new Intent(SplashActivity.this,
+                                LoginActivity.class));
+                    }else{
+
+                        finish();
+                    }
+                }
+                    finish();
+                }
+            };
+        };
+
+        splashTread.start();
+    }
+}
